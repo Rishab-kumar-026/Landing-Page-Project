8801b38889de0883f774daca827dadf8c59d3d34
Rishab-kumar-026 <rks2034287@gmail.com>
2024-04-29 21:32:49 +0530

    Project

---
+++ script1.js
@@ -0,0 +1,65 @@
+// Responsive Nav Bar
+
+const menu_btn = document.getElementById("menu-btn");
+const nav_links = document.getElementById("nav-links");
+const menu_btn_icon = menu_btn.querySelector("i");
+
+menu_btn.addEventListener("click",()=>{
+    nav_links.classList.toggle("show_links");
+
+    if(nav_links.classList.contains("show_links")){
+        menu_btn_icon.classList.remove("fa-bars");
+        menu_btn_icon.classList.add("fa-xmark");
+    }
+    else{
+        menu_btn_icon.classList.add("fa-bars");
+        menu_btn_icon.classList.remove("fa-xmark");
+    }
+})
+
+nav_links.addEventListener("click",()=>{
+    nav_links.classList.remove("show_links");
+    menu_btn_icon.classList.remove("fa-xmark");
+    menu_btn_icon.classList.add("fa-bars");
+
+})
+
+//Header 
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".heading_container p", {
+    ...scrollRevealOption,
+  });
+  
+  ScrollReveal().reveal(".heading_container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+//About 
+
+ScrollReveal().reveal(".about-section img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+  ScrollReveal().reveal(".about-content .section_subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".about-content .section_header", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".about-content .section_description", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  ScrollReveal().reveal(".about-content .btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+  });
+
